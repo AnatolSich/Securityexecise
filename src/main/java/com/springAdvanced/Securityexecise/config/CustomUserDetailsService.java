787f5705b7bd58ce9589ccd14dd3068a610bd3f8
AnatolSich <tip-77@ukr.net>
2020-11-29 15:59:46 +0200

    Formatted, added roles2

--- src/main/java/com/springAdvanced/Securityexecise/config/CustomUserDetailsService.java
+++ src/main/java/com/springAdvanced/Securityexecise/config/CustomUserDetailsService.java
@@ -13,20 +13,18 @@ import java.util.List;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		List<SimpleGrantedAuthority> roles = null;
-		if(username.equals("admin"))
-		{
-			roles=Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
-			return new User("admin","$2a$10$0Nv/Qa7m8DdjWpBS2XRZWeP8rWDB7OdScb2grQSRDS9I9fWWlBNG2",roles);
-		}
-		if(username.equals("user"))
-		{
-			roles=Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-			return new User("user","$2a$10$5VikX1NNQFL9f.N7Ta5wVuBL5HuPi7ro5Q3UZYGVOCURwiotGrVCS",roles);
-		}
-		throw new UsernameNotFoundException("User not found with the name "+ username);
-	}
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<SimpleGrantedAuthority> roles = null;
+        if (username.equals("admin")) {
+            roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_BOOKING_MANAGER"));
+            return new User("admin", "$2a$10$0Nv/Qa7m8DdjWpBS2XRZWeP8rWDB7OdScb2grQSRDS9I9fWWlBNG2", roles);
+        }
+        if (username.equals("user")) {
+            roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_RESGISTERED_USER"));
+            return new User("user", "$2a$10$5VikX1NNQFL9f.N7Ta5wVuBL5HuPi7ro5Q3UZYGVOCURwiotGrVCS", roles);
+        }
+        throw new UsernameNotFoundException("User not found with the name " + username);
+    }
 
 }
