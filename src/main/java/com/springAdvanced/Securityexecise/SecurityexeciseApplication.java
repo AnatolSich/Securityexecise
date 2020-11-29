c7548a8f63fbf36b5c91ee58dd47372f60b3d695
AnatolSich <tip-77@ukr.net>
2020-11-29 15:23:11 +0200

    first commit

---
+++ src/main/java/com/springAdvanced/Securityexecise/SecurityexeciseApplication.java
@@ -0,0 +1,13 @@
+package com.springAdvanced.Securityexecise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityexeciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityexeciseApplication.class, args);
+	}
+
+}
